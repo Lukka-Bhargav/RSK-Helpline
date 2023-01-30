b5b4880eca0a4f45689de95da154a42a5eb7ed50
Lukka-Bhargav <imbhargavlukka@gmail.com>
2023-01-30 14:51:06 +0530

    frist commit

--- RSK-Learning/src/app/student/student.component.ts
+++ RSK-Learning/src/app/student/student.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-student',
@@ -11,5 +11,51 @@ export class StudentComponent implements OnInit {
 
   ngOnInit(): void {
   }
+ 
 
+//   public cousins = [
+//     {
+//         no: 1,
+//         name: "Bhargav",
+//         age: 23,
+//         gender: "male",
+//         isAcive: true
+//     },
+//     {
+//         no: 2,
+//         name: "Honey",
+//         age: 17,
+//         gender: "female",
+//         isAcive: false
+//     },
+//     {
+//         no: 3,
+//         name: "Mghana",
+//         age: 20,
+//         gender: "female",
+//         isAcive: false
+//     },
+//     {
+//         no: 4,
+//         name: "Harshi",
+//         age: 17,
+//         gender: "female",
+//         isAcive: false
+//     },
+//     {
+//         no: 5,
+//         name: "Sai",
+//         age: 22,
+//         gender: "male",
+//         isAcive: true
+//     },
+//     {
+//         no: 6,
+//         name: "Bunny",
+//         age: 17,
+//         gender: "male",
+//         isAcive: true
+//     },
+// ];
+// array = ["Bhargav","Honey","Meghana","sai","Harshi","Bunny","Hi","Hello"];
 }
